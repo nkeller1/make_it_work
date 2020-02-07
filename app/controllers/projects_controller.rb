05c4c825edc33b0568d7d17788142443b0e97d2e
Nathan Keller <keller.nathan@gmail.com>
2020-02-07 10:39:01 -0700

    User Story 4 complete.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,6 @@
 class ProjectsController < ApplicationController
   def show
     @project = Project.find(params[:project_id])
+    @average_age = @project.contestants.average(:age)
   end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -3,4 +3,8 @@ class Project <ApplicationRecord
   has_many :contestant_projects
   has_many :contestants, through: :contestant_projects
   belongs_to :challenge
+
+  def self.average_age
+    contestants.average(:age)
+  end
 end
