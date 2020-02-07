a0584db8f8ea8b3dc3e4faac887fa387d5a0bac4
Nathan Keller <keller.nathan@gmail.com>
2020-02-07 10:04:28 -0700

    User Story 2 complete.

---
+++ spec/views/contestants/index_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+RSpec.describe "contestant index page" do
+  it "shows all contesnt information" do
+
+    contestant_1 = Contestant.create(name: "Sam",
+      age: "12",
+      hometown: "Aurora",
+      years_of_experience: "2")
+
+    contestant_2 = Contestant.create(name: "Nelly",
+      age: "68",
+      hometown: "Spain",
+      years_of_experience: "400")
+
+    recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+    furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+
+    recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+    recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
+
+    furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
+    furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
+
+    contestant_1.projects << furniture_challenge.projects
+    contestant_2.projects << recycled_material_challenge.projects
+
+    visit '/contestants'
+
+    expect(page).to have_content(contestant_1.name)
+    expect(page).to have_content(contestant_2.name)
+    expect(page).to have_content("Upholstery Tuxedo")
+    expect(page).to have_content("Litfit")
+    expect(page).to have_content("Projects: Boardfit")
+    expect(page).to have_content("News Chic")
+  end
+end
+# As a visitor,
+# When I visit the contestants index page ("/contestants")
+# I see a list of names of all the contestants
+# And under each contestants name I see a list of the projects (names) that they've been on
+#
+# (e.g.   Kentaro Kameyama
+#         Projects: Litfit, Rug Tuxedo
+#
+#         Jay McCarroll
+#         Projects: LeatherFeather)
--- spec/views/project/show_spec.rb
+++ spec/views/project/show_spec.rb
@@ -17,11 +17,3 @@ RSpec.describe "project show page" do
     expect(page).to have_content("Challenge Theme: Recycled Material")
   end
 end
-
-
-# When I visit a project's show page ("/projects/:id"),
-# I see that project's name and material
-# And I also see the theme of the challenge that this project belongs to.
-# (e.g.    Litfit
-#     Material: Lamp Shade
-#   Challenge Theme: Apartment Furnishings)
