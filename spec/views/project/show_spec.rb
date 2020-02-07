ea696cab37698b62c9de22778b61e397ffb56ebc
Nathan Keller <keller.nathan@gmail.com>
2020-02-07 10:15:54 -0700

    User Story 3 complete.

--- spec/views/project/show_spec.rb
+++ spec/views/project/show_spec.rb
@@ -16,4 +16,32 @@ RSpec.describe "project show page" do
     expect(page).to have_content("Material: Newspaper")
     expect(page).to have_content("Challenge Theme: Recycled Material")
   end
+
+  it "can count the number of projects" do
+    contestant_1 = Contestant.create(name: "Sam",
+      age: "12",
+      hometown: "Aurora",
+      years_of_experience: "2")
+
+    contestant_2 = Contestant.create(name: "Nelly",
+      age: "68",
+      hometown: "Spain",
+      years_of_experience: "400")
+
+    recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+    furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+
+    recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+    recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
+
+    project_1 = furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
+    project_2 = furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
+
+    contestant_1.projects << furniture_challenge.projects
+    contestant_2.projects << recycled_material_challenge.projects
+
+    visit "/projects/#{project_1.id}"
+
+    expect(page).to have_content("Number of Contestants: #{project_1.contestants.length}")
+  end
 end
